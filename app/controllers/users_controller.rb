a22900ebaabc0728775666f9ebbd05e4ad390054
Sanoryel <lucas.leyronas@orange.fr>
2024-03-14 19:17:12 +0100

    hash for user parameters

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+    @user = User.new(username: params[:user][:username], email: params[:user][:email], password: params[:user][:password])
     p User.all
     if @user.save
       redirect_to new_user_path
